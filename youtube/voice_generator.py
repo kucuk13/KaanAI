654a2066300faa13e15af87cfd2b7e0544228c9a
kucuk13 <52529079+kucuk13@users.noreply.github.com>
2025-03-02 14:04:20 +0300

    Add voice generation functionality with text input and create initial video script

--- youtube/voice_generator.py
+++ youtube/voice_generator.py
@@ -1,2 +1,27 @@
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play, save
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import api_key
+
+def generate_voice_with_text(text, output_path):
+    client = ElevenLabs(
+      api_key=api_key.get("eleven-labs-api-key"),
+    )
+
+    voice = client.text_to_speech.convert(
+        text=text,
+        voice_id="JBFqnCBsd6RMkjVDRZzb",
+        model_id="eleven_multilingual_v2",
+        output_format="mp3_44100_128",
+    )
+    #play(speech_response)
+    save(voice,output_path)
+
 def create_voices(text_parts):
-    print("Creating voices...")
+    for i, part in enumerate(text_parts):
+        output_filename = f"youtube/output/voices/output_part_{i+1}.mp3"
+        generate_voice_with_text(part.strip(), output_filename)
