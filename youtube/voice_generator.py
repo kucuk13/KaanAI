9d17053e7841480da9b9c2534e608fa3545ac9da
kucuk13 <52529079+kucuk13@users.noreply.github.com>
2025-04-06 13:55:25 +0300

    Update text_parts handling and implement voice generation using ChatGPT API

--- youtube/voice_generator.py
+++ youtube/voice_generator.py
@@ -1,5 +1,6 @@
 from elevenlabs.client import ElevenLabs
 from elevenlabs import save
+from openai import OpenAI
 
 import os
 import sys
@@ -8,6 +9,9 @@ import api_key
 
 eleven_labs_amelia = "ZF6FPAbjXT4488VcRRnw"
 eleven_labs_adam = "wBXNqKUATyqu0RtYt25i"
+chat_gpt_young_woman = "nova"
+chat_gpt_middle_aged_woman = "alloy"
+chat_gpt_middle_aged_man = "onyx"
 
 def generate_voice_with_text_using_eleven_labs_api(text, output_path):
     client = ElevenLabs(
@@ -22,8 +26,30 @@ def generate_voice_with_text_using_eleven_labs_api(text, output_path):
     )
     save(voice, output_path)
 
-def generate_voice_with_text_using_chatgpt_api(text, output_path):
-    return
+def generate_voice_with_text_using_chatgpt_api(text, output_filename):
+    voice = ""
+    client = OpenAI(api_key = api_key.get("chat-gpt-api-key"))
+    
+    if "X:" in text:
+        voice = chat_gpt_young_woman
+    else:
+        voice = chat_gpt_middle_aged_man
+    
+    text = text.replace("X:", "")
+    text = text.replace("O:", "")
+
+    try:
+        with client.audio.speech.with_streaming_response.create(
+            model="tts-1",
+            voice=voice,
+            input=text
+        ) as response:
+            temp_file = output_filename
+            with open(temp_file, "wb") as f:
+                for chunk in response.iter_bytes():
+                    f.write(chunk)
+    except Exception as e:
+        print(f".Error: {e}")
 
 def create_voices(text_parts, is_short_video):
     for i, part in enumerate(text_parts):
@@ -32,5 +58,3 @@ def create_voices(text_parts, is_short_video):
             generate_voice_with_text_using_chatgpt_api(part.strip(), output_filename)
         else:
             generate_voice_with_text_using_eleven_labs_api(part.strip(), output_filename)
-    
-        
