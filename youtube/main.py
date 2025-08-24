2a9b05b3e983750e9b487f7bcb1d64301a5de95a
kucuk13 <52529079+kucuk13@users.noreply.github.com>
2025-08-24 14:37:06 +0300

    Add new modules for data, image, video, and voice management with initial implementations

--- youtube/main.py
+++ youtube/main.py
@@ -1,8 +1,8 @@
-import image_generator
-import voice_generator
-import video_generator
+import youtube.image_manager as image_manager
+import youtube.voice_manager as voice_manager
+import youtube.video_manager as video_manager
 import file_manager
-import data_generator
+import youtube.data_manager as data_manager
 
 #user inputs
 is_test = True
@@ -17,29 +17,29 @@ file_manager.create_directory_if_not_exists("youtube/output/videos")
 def create_story_video():
     text_content = file_manager.get_text_content_for_youtube(is_test, False)
     text_parts = text_content.split("***")
-    image_generator.create_images(text_parts)
-    voice_generator.create_voices(text_parts, True)
-    video_generator.create_video_parts(False)
-    video_generator.merge_video_parts("youtube/input/outro.mp4")
+    image_manager.create_images(text_parts)
+    voice_manager.create_voices(text_parts, True)
+    video_manager.create_video_parts(False)
+    video_manager.merge_video_parts("youtube/input/outro.mp4")
 
 def create_shorts():
     text_content = file_manager.get_text_content_for_youtube(is_test, True)
     text_parts = text_content.splitlines()
-    voice_generator.create_voices(text_parts, True)
-    video_generator.create_video_parts(True)
-    video_generator.merge_video_parts("youtube/input/shorts_outro.mp4")
+    voice_manager.create_voices(text_parts, True)
+    video_manager.create_video_parts(True)
+    video_manager.merge_video_parts("youtube/input/shorts_outro.mp4")
 
 def create_questions_and_answers_video():
     text_content = file_manager.get_text_content_for_youtube(is_test, True)
     text_parts = text_content.splitlines()
-    voice_generator.create_voices(text_parts, True)
-    #voice_generator.clean_voice_files()
-    video_generator.create_video_parts(True)
-    video_generator.merge_video_parts("youtube/input/questions_and_answers_outro.mp4")
-    video_generator.change_video_speed("youtube/output/output.mp4", "youtube/output/video.mp4", 0.9)
+    voice_manager.create_voices(text_parts, True)
+    #voice_manager.clean_voice_files()
+    video_manager.create_video_parts(True)
+    video_manager.merge_video_parts("youtube/input/questions_and_answers_outro.mp4")
+    video_manager.change_video_speed("youtube/output/output.mp4", "youtube/output/video.mp4", 0.9)
 
 #main methods
-#data_generator.create_bulk_data()
+#data_manager.create_bulk_data()
 #create_story_video()
 #create_shorts()
 create_questions_and_answers_video()
